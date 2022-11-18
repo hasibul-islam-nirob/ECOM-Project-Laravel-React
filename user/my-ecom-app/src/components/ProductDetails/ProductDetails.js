819f1eab2303499c674c8746f83bf59852b1e262
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-11-18 17:37:38 +0600

    Dynamic Product Details

--- user/my-ecom-app/src/components/ProductDetails/ProductDetails.js
+++ user/my-ecom-app/src/components/ProductDetails/ProductDetails.js
@@ -1,7 +1,8 @@
 import React, {Component, Fragment} from 'react';
 import {Breadcrumb, Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
-
+import ReactHtmlParser from "react-html-parser";
+import ReactDOM from 'react-dom';
 class ProductDetails extends Component {
 
     constructor() {
@@ -23,16 +24,78 @@ class ProductDetails extends Component {
         }
     }
 
+    imgOnClick =(event)=>{
+        let imgSrc = event.target.getAttribute('src');
+        let ImagePreview = document.getElementById('ImagePreview');
+        ReactDOM.findDOMNode(ImagePreview).setAttribute('src',imgSrc);
+    }
+
+    PriceOption =(special_price, price)=>{
+        if (special_price==="NA"){
+            return(
+                <p className="product-price-on-card">Price: { price}TK</p>
+            )
+        }else{
+            return(
+                <p className="product-price-on-card">
+                    Price: <strike class="text-secondary">{ price}TK</strike>  { special_price} TK
+                </p>
+            )
+        }
+    }
+
+
+
     render() {
 
         let ProductData = this.props.ProductData;
         let title = ProductData['ProductList'][0]['title'];
         let productBigImg = ProductData['ProductList'][0]['image'];
+        let price = ProductData['ProductList'][0]['price'];
+        let special_price = ProductData['ProductList'][0]['special_price'];
+        let category = ProductData['ProductList'][0]['category'];
+        let sub_category = ProductData['ProductList'][0]['sub_category'];
+        let remark = ProductData['ProductList'][0]['remark'];
+        let brand = ProductData['ProductList'][0]['brand'];
+        let star = ProductData['ProductList'][0]['star'];
+        let product_code = ProductData['ProductList'][0]['product_code'];
+
         let des = ProductData['ProductDetails'][0]['des'];
+        let details = ProductData['ProductDetails'][0]['details'];
+        let size = ProductData['ProductDetails'][0]['size'];
+        let color = ProductData['ProductDetails'][0]['color'];
         let img1 = ProductData['ProductDetails'][0]['img1'];
         let img2 = ProductData['ProductDetails'][0]['img2'];
         let img3 = ProductData['ProductDetails'][0]['img3'];
         let img4 = ProductData['ProductDetails'][0]['img4'];
+
+        // ========== Working With Color Start ===========//
+        var ColorDiv="d-none"
+        if(color!=="NA"){
+            let ColorArray = color.split(',');
+            var ColorOption=ColorArray.map((ColorList,i)=>{
+                return <option value={ColorList}>{ColorList}</option>
+            })
+            ColorDiv=""
+        }
+        else{
+            ColorDiv="d-none"
+        }
+        // ========== Working With Color End ===========//
+
+        // ========== Working With Size Start ===========//
+        var SizeDiv="d-none"
+        if(size!=="NA"){
+            let SizeArray = size.split(',');
+            var  SizeOption=SizeArray.map((SizeList,i)=>{
+                return <option value={SizeList}>{SizeList}</option>
+            })
+            SizeDiv=""
+        }
+        else{
+            SizeDiv="d-none"
+        }
+        // ========== Working With Size End ===========//
 
         return (
             <Fragment>
@@ -48,20 +111,20 @@ class ProductDetails extends Component {
                             <Row>
                                 <Col className="p-3 animated " md={6} lg={6} sm={12} xs={12}>
 
-                                    <img className="w-100" src={productBigImg}  alt=""/>
+                                    <img className="w-100" id="ImagePreview" src={productBigImg} height="450px"  alt=""/>
                                     <Container className="my-3">
                                         <Row>
                                             <Col className="p-1 image-box m-0"  md={3} lg={3} sm={3} xs={3}>
-                                                <img className="w-100 Product-sm-img" src={img1} alt=""/>
+                                                <img onClick={this.imgOnClick} className="w-100 Product-sm-img" src={img1} alt=""/>
                                             </Col>
                                             <Col className="p-1 image-box m-0" md={3} lg={3} sm={3} xs={3}>
-                                                <img className="w-100 Product-sm-img" src={img1} alt=""/>
+                                                <img onClick={this.imgOnClick} className="w-100 Product-sm-img" src={img2} alt=""/>
                                             </Col>
                                             <Col className="p-1 image-box m-0" md={3} lg={3} sm={3} xs={3}>
-                                                <img className="w-100 Product-sm-img" src={img1} alt=""/>
+                                                <img onClick={this.imgOnClick} className="w-100 Product-sm-img" src={img3} alt=""/>
                                             </Col>
                                             <Col className="p-1 image-box m-0" md={3} lg={3} sm={3} xs={3}>
-                                                <img className="w-100 Product-sm-img" src={img1} alt=""/>
+                                                <img onClick={this.imgOnClick} className="w-100 Product-sm-img" src={img4} alt=""/>
                                             </Col>
                                         </Row>
                                     </Container>
@@ -69,21 +132,24 @@ class ProductDetails extends Component {
                                 <Col className="p-3 " md={6} lg={6} sm={12} xs={12}>
                                     <h5 className="Product-Name">{title}</h5>
                                     <h6 className="section-sub-title">{des}</h6>
-                                    <h5>Price: 150$</h5>
+                                    <h5>{this.PriceOption(special_price, price)}</h5>
 
-                                    <div className="">
+                                    <div className={ColorDiv}>
                                         <h6 className="mt-2">Choose Color</h6>
-                                        <select className="form-control form-select">
-                                            <option value="">Choose Color</option>
+                                        <select onChange={this.colorOnChange} className="form-control form-select">
+                                            <option value="" disabled selected>Choose Color</option>
+                                            {ColorOption}
                                         </select>
                                     </div>
 
-                                    <div className="">
+                                    <div className={SizeDiv}>
                                         <h6 className="mt-2">Choose Size</h6>
-                                        <select  className="form-control form-select">
+                                        <select onChange={this.sizeOnChange} className="form-control form-select">
                                             <option value="">Choose Size</option>
+                                            {SizeOption}
                                         </select>
                                     </div>
+
                                     <div className="">
                                         <h6 className="mt-2">Choose Quantity</h6>
                                         <select  className="form-control form-select">
@@ -100,6 +166,7 @@ class ProductDetails extends Component {
                                             <option value="10">10</option>
                                         </select>
                                     </div>
+
                                     <div className="input-group mt-3">
                                         <button  className="btn site-btn m-1 "> <i className="fa fa-shopping-cart"/> {this.state.addToCart}</button>
                                         <button  className="btn btn-primary m-1"> <i className="fa fa-car"/>{this.state.orderNow}</button>
@@ -113,9 +180,8 @@ class ProductDetails extends Component {
                         <Col className="p-1" md={6} lg={6} sm={12} xs={12}>
                             <div className="shadow-sm p-3 bg-white">
                                 <h6 className="mt-2">DETAILS</h6>
-                                <p>consectetur adipisicing elit. Consequatur laudantium modi quaerat quo. Consequuntur harum odit provident, quis tempora totam vitae. Animi dignissimos dolores itaque magnam nulla odit similique tempore!</p>
-                                <p>consectetur adipisicing elit. Consequatur laudantium modi quaerat quo. Consequuntur harum odit provident, quis tempora totam vitae. Animi dignissimos dolores itaque magnam nulla odit similique tempore!</p>
-                                <p>consectetur adipisicing elit. Consequatur laudantium modi quaerat quo. Consequuntur harum odit provident, quis tempora totam vitae. Animi dignissimos dolores itaque magnam nulla odit similique tempore!</p>
+
+                                {ReactHtmlParser(details)}
                             </div>
                         </Col>
                         <Col className="p-1" md={6} lg={6} sm={12} xs={12}>
