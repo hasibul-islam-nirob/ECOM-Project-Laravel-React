12f7e269c9342f3334cd0a06b0279f7d2a05229a
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-11-22 04:25:38 +0600

    Create OTP Table

--- admin/ecom-admin/app/Models/OTPModel.php
+++ admin/ecom-admin/app/Models/OTPModel.php
@@ -5,14 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SettingsModel extends Model
+class OTPModel extends Model
 {
     use HasFactory;
 
-    public $table='settings';
+    public $table='otp_table';
     public $primaryKey='id';
     public $incrementing=true;
     public $keyType='int';
     public  $timestamps=false;
-
 }
