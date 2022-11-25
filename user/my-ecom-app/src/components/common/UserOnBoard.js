47ee748225ff6299fff84b5f4256ee7ce0a16b90
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-11-26 01:28:40 +0600

    Validation Mobile Number

--- user/my-ecom-app/src/components/common/UserOnBoard.js
+++ user/my-ecom-app/src/components/common/UserOnBoard.js
@@ -1,5 +1,10 @@
 import React, {Component, Fragment} from 'react';
 import {Card, Col, Container, Form, Row} from "react-bootstrap";
+import validation from '../../velidation/Validation'
+import {Redirect} from "react-router";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 class UserOnBoard extends Component {
     constructor() {
@@ -9,34 +14,64 @@ class UserOnBoard extends Component {
             MobileNo:"",
             UserRedirect:false
         }
-        
+        this.MobileOnChange=this.MobileOnChange.bind(this);
+        this.onNextClick=this.onNextClick.bind(this);
     }
+
+    MobileOnChange(event){
+        let mobile=  event.target.value;
+        this.setState({MobileNo:mobile});
+
+    }
+
+    onNextClick(){
+        let mobile= this.state.MobileNo;
+        if(mobile.length===0){
+            toast.error('Mobile Number Required', {position: "bottom-center"});
+        }
+        else if(!(validation.mobileRegx).test(mobile)){
+            toast.error("Invalid Mobile Number",{position:'bottom-center'});
+        }
+        else {
+            alert(mobile)
+        }
+    }
+
+
 
     render() {
         return (
             <Fragment>
-                <Container className="py-5 TopSection">
-                    <Row className="d-flex justify-content-center pt-5">
-                        <Col md={4} lg={4} sm={8} xs={12}>
-                            <Card style={{ width: '18rem' }} >
-                                <Card.Body >
-                                    <Card.Title className="py-3 text-center">User Login</Card.Title>
-                                    <Card.Text>
-                                        <Form>
-                                            <Form.Group controlId="formBasicEmail">
-                                                <Form.Label><b>Please Enter Your Phone Number</b></Form.Label>
-                                                <Form.Control type="text" placeholder=" " />
-                                            </Form.Group>
-                                        </Form>
-                                    </Card.Text>
-                                    <div className="d-grid gap-2 pt-2 pb-5">
-                                        <button className="btn btn-info" type="button"> Next </button>
+                <Container className="TopSection animated slideInDown">
+                    <Row className="d-flex p-1 justify-content-center">
+                        <Col className="shadow-sm bg-white mt-2 justify-content-center" md={6} lg={6} sm={12} xs={12}>
+                            <Row className="text-center ">
+                                <Col className=" justify-content-center" md={12} lg={12} sm={12} xs={12}>
+                                    <div className="onboardForm">
+                                        <h4 className="section-title">USER SIGN IN</h4>
+                                        <h6 className="section-sub-title">Please Enter Your Mobile No, And Go Next</h6>
+                                        <input onChange={this.MobileOnChange} className="form-control m-2" type="text" placeholder="Mobile Number"/>
+                                        <button onClick={this.onNextClick} className="btn btn-block w-100 m-2 site-btn">{this.state.btn}</button>
                                     </div>
-                                </Card.Body>
-                            </Card>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
+
+                <ToastContainer
+                    position="bottom-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    //theme="light"
+                    theme="colored"
+                />
             </Fragment>
         );
     }
