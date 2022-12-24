fde07c49ac93c9df1da3a8a853b0f6233776e515
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-12-24 22:45:33 +0600

    change updated phone regx

--- user/my-ecom-app/src/velidation/Validation.js
+++ user/my-ecom-app/src/velidation/Validation.js
@@ -2,7 +2,7 @@
 class Validation{
     static msgRegx = /^([a-zA-Z., ]){2,500}$/;
     static nameRegx = /^([a-zA-Z ]){2,20}$/;
-    static mobileRegx = /^(?:\+?88|0088)?01[15-9]\d{8}$/; // for bangladesh
+    static mobileRegx = /^(?:\+88|01)?\d{11}\r?$/; // for bangladesh
     static emailRegx = /\S+@\S+\.\S+/;
 
 }
